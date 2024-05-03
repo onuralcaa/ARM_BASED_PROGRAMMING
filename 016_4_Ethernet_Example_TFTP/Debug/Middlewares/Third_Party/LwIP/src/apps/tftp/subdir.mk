a02958a9a0340b15c094749e87904f2c121f5ff4
Onur ALACA <onuralcaa@gmail.com>
2024-05-03 22:54:10 +0300

    TFTP application

---
+++ 016_4_Ethernet_Example_TFTP/Debug/Middlewares/Third_Party/LwIP/src/apps/tftp/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/Third_Party/LwIP/src/apps/tftp/tftp_server.c 
+
+OBJS += \
+./Middlewares/Third_Party/LwIP/src/apps/tftp/tftp_server.o 
+
+C_DEPS += \
+./Middlewares/Third_Party/LwIP/src/apps/tftp/tftp_server.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/Third_Party/LwIP/src/apps/tftp/%.o Middlewares/Third_Party/LwIP/src/apps/tftp/%.su Middlewares/Third_Party/LwIP/src/apps/tftp/%.cyclo: ../Middlewares/Third_Party/LwIP/src/apps/tftp/%.c Middlewares/Third_Party/LwIP/src/apps/tftp/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Middlewares/Third_Party/LwIP/src/include/lwip/priv -I../Middlewares/Third_Party/LwIP/src/include/netif/ppp -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Middlewares/Third_Party/LwIP/src/include/compat/stdc -I../Middlewares/Third_Party/LwIP/src/include/lwip/prot -I../Middlewares/Third_Party/LwIP/src/include/lwip/apps -I../Middlewares/Third_Party/LwIP/src/include/compat/posix/sys -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/LwIP/system/arch -I../Middlewares/Third_Party/LwIP/src/include -I../LWIP/App -I../Middlewares/Third_Party/LwIP/src/include/compat/posix -I../Drivers/CMSIS/Include -I../Core/Inc -I../LWIP/Target -I../Middlewares/Third_Party/LwIP/src/include/lwip -I../Middlewares/Third_Party/LwIP/src/include/compat/posix/net -I../Middlewares/Third_Party/LwIP/src/include/compat/posix/arpa -I../Middlewares/Third_Party/LwIP/system -I../Middlewares/Third_Party/LwIP/src/include/netif -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Middlewares-2f-Third_Party-2f-LwIP-2f-src-2f-apps-2f-tftp
+
+clean-Middlewares-2f-Third_Party-2f-LwIP-2f-src-2f-apps-2f-tftp:
+	-$(RM) ./Middlewares/Third_Party/LwIP/src/apps/tftp/tftp_server.cyclo ./Middlewares/Third_Party/LwIP/src/apps/tftp/tftp_server.d ./Middlewares/Third_Party/LwIP/src/apps/tftp/tftp_server.o ./Middlewares/Third_Party/LwIP/src/apps/tftp/tftp_server.su
+
+.PHONY: clean-Middlewares-2f-Third_Party-2f-LwIP-2f-src-2f-apps-2f-tftp
+
